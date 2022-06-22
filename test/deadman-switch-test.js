1b5c262cdb99a925e708c550e0c749c49685c694
MahikaJaguste <mahika.oj@iitgn.ac.in>
2022-06-22 19:50:25 +0530

    Added test to check transfer of funds

--- test/deadman-switch-test.js
+++ test/deadman-switch-test.js
@@ -8,12 +8,11 @@ describe("DeadmanSwitch contract", function () {
     let owner;
     let alice;
     let bob;
-    let charlie;
     let blockWhenDeployed;
 
     beforeEach(async () => {
         DeadmanSwitch = await ethers.getContractFactory("DeadmanSwitch");
-        [owner, alice, bob, charlie] = await ethers.getSigners();
+        [owner, alice, bob] = await ethers.getSigners();
         deadmanSwitch = await DeadmanSwitch.deploy(alice.address);
         blockWhenDeployed = await ethers.provider.getBlockNumber();
     });
@@ -81,7 +80,25 @@ describe("DeadmanSwitch contract", function () {
     });
 
     it("should transfer funds when conditions are satisfied", async () => {
+        await deadmanSwitch.still_alive();
+        const blockWhenCalled = await ethers.provider.getBlockNumber();
+        tx = {
+            to: deadmanSwitch.address,
+            value: ethers.utils.parseEther('1', 'ether'),
+        };
+        await owner.sendTransaction(tx);
+        await hre.network.provider.send("hardhat_mine", ["0xF"]);
 
+        const currentBlock = await ethers.provider.getBlockNumber();
+        assert.isTrue(currentBlock - blockWhenCalled > 10);
+        expect(await ethers.provider.getBalance(deadmanSwitch.address)).to.equal(ethers.utils.parseEther('1'));
+        
+        const b1 = await ethers.provider.getBalance(alice.address);
+        await deadmanSwitch.transferFunds();
+        const b2 = await ethers.provider.getBalance(alice.address);
+
+        expect(b2.sub(b1)).to.equal(ethers.utils.parseEther('1'));
+        expect(await ethers.provider.getBalance(deadmanSwitch.address)).to.equal(ethers.utils.parseEther('0'));
     })
 
 });
